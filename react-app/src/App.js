fc1e204d34ed2c583fc5e4e900f98ec616ef8bc7
pupapupa12 <152703819+pupapupa12@users.noreply.github.com>
2024-06-01 21:15:23 +0900

    React_chap4
    컴포넌트로 수정

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,30 +1,48 @@
 import "./App.css";
 
+function Header() {
+    return (
+        <header>
+            <h1>
+                <a href="/">WEB</a>
+            </h1>
+        </header>
+    );
+}
+
+function Navigation() {
+    return (
+        <nav>
+            <ol>
+                <li>
+                    <a href="/read/1">html</a>
+                </li>
+                <li>
+                    <a href="/read/2">css</a>
+                </li>
+                <li>
+                    <a href="/read/3">js</a>
+                </li>
+            </ol>
+        </nav>
+    );
+}
+
+function Article() {
+    return (
+        <article>
+            <h2>Welcome</h2>
+            Hello,WEB
+        </article>
+    );
+}
+
 function App() {
     return (
         <div>
-            <header>
-                <h1>
-                    <a href="/">Web</a>
-                </h1>
-            </header>
-            <nav>
-                <ol>
-                    <li>
-                        <a href="/read/1">html</a>
-                    </li>
-                    <li>
-                        <a href="/read/2">css</a>
-                    </li>
-                    <li>
-                        <a href="/read/3">js</a>
-                    </li>
-                </ol>
-            </nav>
-            <article>
-                <h2>Welcome</h2>
-                Hello,WEB
-            </article>
+            <Header></Header>
+            <Navigation></Navigation>
+            <Article></Article>
         </div>
     );
 }
