a620f72cd5244fc46a2d0c9d9db060eca4474f88
pupapupa12 <152703819+pupapupa12@users.noreply.github.com>
2024-06-01 22:30:34 +0900

    React_5.Props
    아놔 li 풀어서 쓰는 거 하나도 모르겟숨요~~

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,48 +1,58 @@
 import "./App.css";
 
-function Header() {
+function Header(props) {
+    // console.log("props", props, props.title);
     return (
         <header>
             <h1>
-                <a href="/">WEB</a>
+                <a href="/">{props.title}</a>
             </h1>
         </header>
     );
 }
 
-function Navigation() {
+function Navigation(props) {
+    const lis = [
+        // <li><a href="/read/1">html</a></li>,
+        // <li><a href="/read/2">css</a></li>,
+        // <li><a href="/read/3">js</a></li>,
+    ];
+    for (let i = 0; i < props.topics.length; i++) {
+        let t = props.topics[i];
+        lis.push(
+            <li key={t.id}>
+                <a href={"/read/" + t.id}>{t.title}</a>
+            </li>
+        );
+    }
     return (
         <nav>
-            <ol>
-                <li>
-                    <a href="/read/1">html</a>
-                </li>
-                <li>
-                    <a href="/read/2">css</a>
-                </li>
-                <li>
-                    <a href="/read/3">js</a>
-                </li>
-            </ol>
+            <ol>{lis}</ol>
         </nav>
     );
 }
 
-function Article() {
+function Article(props) {
     return (
         <article>
-            <h2>Welcome</h2>
-            Hello,WEB
+            <h2>{props.title}</h2>
+            {props.body}
         </article>
     );
 }
 
 function App() {
+    const topics = [
+        { id: 1, title: "html", body: "html is ..." },
+        { id: 2, title: "css", body: "css is ..." },
+        { id: 3, title: "js", body: "js is ..." },
+    ];
     return (
         <div>
-            <Header></Header>
-            <Navigation></Navigation>
-            <Article></Article>
+            <Header title="REACT"></Header>
+            <Navigation topics={topics}></Navigation>
+            <Article title="Welcome" body="Hello, WEB"></Article>
+            {/* <Article title="Hi" body="Hello, React"></Article> */}
         </div>
     );
 }
